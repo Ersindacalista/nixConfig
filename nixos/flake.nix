30b4189b13de3ab91086900dc03f1bd2abe474f4
Ersindacalista <153999229+Ersindacalista@users.noreply.github.com>
2026-05-11 10:22:26 +0200

    Add files via upload

---
+++ nixos/flake.nix
@@ -0,0 +1,33 @@
+{
+    description = " Hyperland on nixos";
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { nixpkgs, home-manager, ... }: {
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+               ./configuration.nix
+               home-manager.nixosModules.home-manager
+               {
+                   home-manager = {
+                       useGlobalPkgs = true;
+                       useUserPackages = true;
+                       users.gabriele = import ./home.nix;
+                       backupFileExtension = "backup";
+                   };
+               }        
+            ];
+        };
+    };
+
+}
+
+
+
+
